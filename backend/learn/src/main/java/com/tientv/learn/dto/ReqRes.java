896e66e788fc2af34201fcea9bceaa68fd67d853
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-13 15:22:26 +0700

    Update ReqRes.java

--- backend/learn/src/main/java/com/tientv/learn/dto/ReqRes.java
+++ backend/learn/src/main/java/com/tientv/learn/dto/ReqRes.java
@@ -19,6 +19,7 @@ public class ReqRes {
     private String name;
     private String city;
     private String role;
+    private String email;
     private String password;
     private OurUsers ourUsers;
     private List<OurUsers> ourUsersList;
