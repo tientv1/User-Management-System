187686f41602c68246f950ce4c0998605f1ea5a0
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-13 15:44:02 +0700

    Update JWTUtils.java

--- backend/learn/src/main/java/com/tientv/learn/service/JWTUtils.java
+++ backend/learn/src/main/java/com/tientv/learn/service/JWTUtils.java
@@ -1,5 +1,6 @@
 package com.tientv.learn.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -10,15 +11,16 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.function.Function;
 
 @Component
 public class JWTUtils {
-    private SecretKey key;
+    private final SecretKey Key;
     private static final long EXPIRATION_TIME = 86400000; // <-- 24 Hours
     public JWTUtils() {
         String secretString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
         byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
-        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+        this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
     }
 
     public String generateToken(UserDetails userDetail) {
@@ -26,7 +28,7 @@ public class JWTUtils {
                 .subject(userDetail.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
+                .signWith(Key)
                 .compact();
     }
 
@@ -36,7 +38,24 @@ public class JWTUtils {
                 .subject(userDetail.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
+                .signWith(Key)
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(Jwts.parser().verifyWith(Key).build().parseSignedClaims(token).getPayload());
+    }
+
+    public boolean isValidateToken(String token, UserDetails userDetail) {
+        final String username = extractUsername(token);
+        return(username.equals(userDetail.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
     }
 }
