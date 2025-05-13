dfd85027b9f33b538219935574a60cebdbdbcfa7
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-13 20:07:01 +0700

    Update UserManagementService.java

--- backend/learn/src/main/java/com/tientv/learn/service/UserManagementService.java
+++ backend/learn/src/main/java/com/tientv/learn/service/UserManagementService.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
