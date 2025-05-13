75a531c8b5d05b4c69d9ec7d93a794d72b76ccf8
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-13 16:44:08 +0700

    Create UserManagementService.java

---
+++ backend/learn/src/main/java/com/tientv/learn/service/UserManagementService.java
@@ -0,0 +1,94 @@
+package com.tientv.learn.service;
+
+import com.tientv.learn.dto.ReqRes;
+import com.tientv.learn.entity.OurUsers;
+import com.tientv.learn.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class UserManagementService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public ReqRes register(ReqRes registrationRequest) {
+        ReqRes resp = new ReqRes();
+        try {
+            OurUsers ourUsers = new OurUsers();
+            ourUsers.setEmail(registrationRequest.getEmail());
+            ourUsers.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+            ourUsers.setRole(registrationRequest.getRole());
+            ourUsers.setName(registrationRequest.getName());
+            ourUsers.setCity(registrationRequest.getCity());
+            OurUsers ourUsersResult = usersRepository.save(ourUsers);
+            if (ourUsersResult.getId() > 0) {
+                resp.setOurUsers(ourUsersResult);
+                resp.setMessage("User Saved Successfully");
+                resp.setStatusCode(200);
+            }
+        } catch (Exception e) {
+            resp.setStatusCode(500);
+            resp.setError(e.getMessage());
+        }
+        return resp;
+    }
+
+    public ReqRes login(ReqRes loginRequest) {
+        ReqRes response = new ReqRes();
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+            );
+            var user = usersRepository.findByEmail(loginRequest.getEmail()).orElseThrow();
+            var jwt = jwtUtils.generateToken(user);
+            var refreshJwt = jwtUtils.generateRefreshToken(new HashMap<>(), user);
+            response.setStatusCode(200);
+            response.setToken(jwt);
+            response.setRefreshToken(refreshJwt);
+            response.setExpirationTime("24 Hours");
+            response.setMessage("Login Successfully");
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setError(e.getMessage());
+        }
+        return response;
+    }
+
+    public ReqRes refreshToken(ReqRes refreshTokenReqiest){
+        ReqRes response = new ReqRes();
+        try{
+            String ourEmail = jwtUtils.extractUsername(refreshTokenReqiest.getToken());
+            OurUsers users = usersRepository.findByEmail(ourEmail).orElseThrow();
+            if (jwtUtils.isTokenValid(refreshTokenReqiest.getToken(), users)) {
+                var jwt = jwtUtils.generateToken(users);
+                response.setStatusCode(200);
+                response.setToken(jwt);
+                response.setRefreshToken(refreshTokenReqiest.getToken());
+                response.setExpirationTime("24Hr");
+                response.setMessage("Successfully Refreshed Token");
+            }
+            response.setStatusCode(200);
+            return response;
+
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage(e.getMessage());
+            return response;
+        }
+    }
+}
