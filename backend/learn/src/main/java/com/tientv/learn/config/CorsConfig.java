dbc7f0f6d9d5f133bec7c6af51206065ce803615
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-13 17:36:39 +0700

    Update CorsConfig.java

--- backend/learn/src/main/java/com/tientv/learn/config/CorsConfig.java
+++ backend/learn/src/main/java/com/tientv/learn/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedOrigins("*");
             }
         };
