390e79bfdd19a288da6927cdaa8015cd9155b7b0
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-13 15:34:23 +0700

    Create JWTUtils.java

---
+++ backend/learn/src/main/java/com/tientv/learn/service/JWTUtils.java
@@ -0,0 +1,42 @@
+package com.tientv.learn.service;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Objects;
+
+@Component
+public class JWTUtils {
+    private SecretKey key;
+    private static final long EXPIRATION_TIME = 86400000; // <-- 24 Hours
+    public JWTUtils() {
+        String secretString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
+        byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generateToken(UserDetails userDetail) {
+        return Jwts.builder()
+                .subject(userDetail.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String generateRefreshToken(HashMap<String, Objects> claims, UserDetails userDetail) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetail.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+}
