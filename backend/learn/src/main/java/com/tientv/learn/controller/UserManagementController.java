b4d155a19877ab200a14ca081374c7a464820a01
Tien Tran Van <tientranvan19052004@gmail.com>
2025-05-13 17:13:34 +0700

    Update UserManagementController.java

--- backend/learn/src/main/java/com/tientv/learn/controller/UserManagementController.java
+++ backend/learn/src/main/java/com/tientv/learn/controller/UserManagementController.java
@@ -1,9 +1,12 @@
 package com.tientv.learn.controller;
 
 import com.tientv.learn.dto.ReqRes;
+import com.tientv.learn.entity.OurUsers;
 import com.tientv.learn.service.UserManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,6 +25,38 @@ public class UserManagementController {
         return ResponseEntity.ok(userManagementService.login(reg));
     }
 
+    @PostMapping("/auth/refresh")
+    public ResponseEntity<ReqRes> refreshToken(@RequestBody ReqRes req){
+        return ResponseEntity.ok(userManagementService.refreshToken(req));
+    }
+
     @GetMapping("/admin/get-all-users")
-    
+    public ResponseEntity<ReqRes> getAllUsers(){
+        return ResponseEntity.ok(userManagementService.getAllUsers());
+
+    }
+
+    @GetMapping("/admin/get-users/{userId}")
+    public ResponseEntity<ReqRes> getUSerByID(@PathVariable Integer userId){
+        return ResponseEntity.ok(userManagementService.getUsersById(userId));
+
+    }
+
+    @PutMapping("/admin/update/{userId}")
+    public ResponseEntity<ReqRes> updateUser(@PathVariable Integer userId, @RequestBody OurUsers reqres){
+        return ResponseEntity.ok(userManagementService.updateUser(userId, reqres));
+    }
+
+    @GetMapping("/adminuser/get-profile")
+    public ResponseEntity<ReqRes> getMyProfile(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        ReqRes response = userManagementService.getMyInfo(email);
+        return  ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @DeleteMapping("/admin/delete/{userId}")
+    public ResponseEntity<ReqRes> deleteUSer(@PathVariable Integer userId){
+        return ResponseEntity.ok(userManagementService.deleteUser(userId));
+    }
 }
